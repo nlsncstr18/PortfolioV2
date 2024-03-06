b332e58c3287921a115150b993c0d4d1acba59a1
Nelson <nlsncstr18@yahoo.com>
2024-03-06 16:30:46 +0800

    fixed

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "/PortfolioV2/",
+  base: "/PortfolioV2",
   plugins: [react()],
 });
